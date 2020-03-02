b753ffa8d2bbfda45a8a20003654d82beb4f7805
lljshuai <328674516@qq.com>
2020-03-02 10:46:09 +0800

    第一次作业

---
+++ Homework01/Calculator01/Calculator01/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // Declare variables and then initialize to zero.
+                float num2 = 0; float num1 = 0;
+                try
+                {
+                    // Ask the user to type the first number.
+                    Console.WriteLine("输入第一个运算数：");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("------------------------");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("非法字符，请输入正确的数字：");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("------------------------");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("数据过大！！");
+                    num1 = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("------------------------");
+                }
+
+
+                try
+                {
+                    // Ask the user to type the first number.
+                    Console.WriteLine("输入第二个运算数：");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("------------------------");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("非法字符，请输入正确的数字：");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("------------------------");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("数据过大！！");
+                    num2 = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("------------------------");
+
+                }
+
+                Console.Write("请输入运算符 + - * / ：");
+
+                // Use a switch statement to do the math.
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        Console.WriteLine($"结果为: {num1} + {num2} = " + (num1 + num2));
+                        break;
+                    case "-":
+                        Console.WriteLine($"结果为: {num1} - {num2} = " + (num1 - num2));
+                        break;
+                    case "*":
+                        try
+                        {
+                            Console.WriteLine($"结果为: {num1} * {num2} = " + (num1 * num2));
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("数据过大！！");
+                        }
+
+                        break;
+                    case "/":
+                        while (num2 == 0)
+                        {
+                            Console.WriteLine("请输入非零数字：");
+                            num2 = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Console.WriteLine($"结果为: {num1} / {num2} = " + (num1 / num2));
+                        break;
+                }
+                Console.WriteLine("------------------------");
+                Console.WriteLine("------------------------");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Homework01/Calculator02/Calculator02/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator02
+{
+    public partial class Form1 : Form
+    {
+        public object Int { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "+";
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("请输入数字后点击+号", "提示", MessageBoxButtons.OK);
+            }
+            else
+            {
+                double r1 = Convert.ToDouble(textBox1.Text);
+                double r2 = Convert.ToDouble(textBox2.Text);
+                textBox3.Text = (Math.Round(r1, 2) + Math.Round(r2, 2)).ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "-";
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("请输入数字后点击-号", "提示", MessageBoxButtons.OK);
+            }
+            else
+            {
+                double r1 = Convert.ToDouble(textBox1.Text);
+                double r2 = Convert.ToDouble(textBox2.Text);
+                textBox3.Text = (Math.Round(r1, 2) - Math.Round(r2, 2)).ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "*";
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("请输入数字后点击*号", "提示", MessageBoxButtons.OK);
+            }
+            else
+            {
+                double r1 = Convert.ToDouble(textBox1.Text);
+                double r2 = Convert.ToDouble(textBox2.Text);
+                textBox3.Text = (Math.Round(r1, 2) * Math.Round(r2, 2)).ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Text = "/";
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("请输入数字后点击/号", "提示", MessageBoxButtons.OK);
+            }
+            else
+            {
+                double r1 = Convert.ToDouble(textBox1.Text);
+                double r2 = Convert.ToDouble(textBox2.Text);
+                textBox3.Text = (Math.Round(r1, 2) / Math.Round(r2, 2)).ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label2.Text = "？";
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+        }
+    }
+}
