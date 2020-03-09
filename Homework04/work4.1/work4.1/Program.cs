d770f3bbff4de443f7914202db69fd241c9b7e14
lljshuai <328674516@qq.com>
2020-03-09 21:25:23 +0800

    第四次作业

---
+++ Homework04/work4.1/work4.1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work4._1
+{
+
+    // 链表节点
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void Foreach(Action<T> action)
+        {
+            Node<T> n = (head == null) ? null : head;
+            action(n.Data);
+            while (n != null)
+            {
+                action(n.Data);
+                n = n.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, max = 0, min = 0;
+            GenericList<int> list = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                list.Add(x);
+            }
+            list.Foreach(x => sum += x);
+            list.Foreach(x => Console.WriteLine(x));
+            list.Foreach(x => max = (max > x) ? max : x);
+            list.Foreach(x => min = (min > x) ? x : min);
+            Console.WriteLine("最大值为："+max);
+            Console.WriteLine("最小值为："+min);
+        }
+    }
+}
---
+++ Homework04/work4.2/work4.2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace work4._2
+{
+    public delegate void TickEventHandler(object a, TickEventArgs args);
+    public class TickEventArgs
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+    public delegate void AlarmEventHandler(object b, AlarmEventArgs args);
+    public class AlarmEventArgs
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Event
+    {
+        public event TickEventHandler TickEvent;
+        public event AlarmEventHandler AlarmEvent;
+
+        public void Tick(int x,int y)
+        {
+            
+            TickEventArgs args = new TickEventArgs()
+            {
+                X = x,
+                Y = y
+            };
+
+            TickEvent(this, args);
+        }
+        public void Alarm(int x, int y)
+        {
+            AlarmEventArgs args = new AlarmEventArgs()
+            {
+                X = x,
+                Y = y
+            };
+
+            AlarmEvent(this, args);
+
+        }
+
+    }
+    public class Form
+    {
+        public Event event1 = new Event();
+        int x=0;int y=0;
+        public Form()
+        {
+            event1.TickEvent += new TickEventHandler(Clock_Tick);
+        }
+
+        void Clock_Tick(object a, TickEventArgs args)
+        {
+            for (y=0; y <= 60; y++)
+            {
+                if (y == 60)
+                {
+                    y = 0; x = x + 1;
+                    Console.WriteLine(x + ":" + y);
+                    Thread.Sleep(500);
+                }
+                else if (x == 0 && y == 10)
+                {
+                    Clock_Alarm( a,  args);
+                }
+                else
+                {
+                    Console.WriteLine(x + ":" + y);
+
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+        void Clock_Alarm(object a, TickEventArgs args)
+        {
+                    Console.WriteLine(x + ":" + y+"   闹钟响了！");
+                    Thread.Sleep(500);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            form1.event1.Tick(0, 0);//模拟点击按钮
+
+        }
+    }
+}
+
